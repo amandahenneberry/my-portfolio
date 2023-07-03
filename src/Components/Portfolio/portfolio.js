1830c51258879ae62f4f3df2f35a9829e8be670e
Amanda Henneberry <amandahenneberry@icloud.com>
2023-07-03 14:43:08 -0400

    updated github portfolio link

--- src/Components/Portfolio/portfolio.js
+++ src/Components/Portfolio/portfolio.js
@@ -1,6 +1,6 @@
 export const portfolio= {
     title: 'Web Developer Portfolio',
     site: 'https://ahenneberry-portfolio.herokuapp.com/',
-    github: 'https://github.com/amandahenneberry/portfolio',
-    blurb: 'blah blah stuufff'
+    github: 'https://github.com/amandahenneberry/my-portfolio',
+    // blurb: 'blah blah stuufff'
 }
