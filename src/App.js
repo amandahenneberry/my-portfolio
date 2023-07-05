c7f6f657707034be4dcdff337a714ca720387b57
Amanda Henneberry <amandahenneberry@icloud.com>
2023-07-05 16:58:21 -0400

    mobile test detect portrait v landscapre

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './index';
 import Home from './Components/Home';
 import MobileHome from './Components/MobileHome';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import { useMediaQuery } from 'react-responsive';
 
@@ -12,6 +12,19 @@ import MediaQuery from 'react-responsive'
 
 
 function App() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth)
+  };
+
+  useEffect (() => {
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, []);
+
+
   return (
     <div>
       <MediaQuery minWidth={992}>
@@ -22,8 +35,14 @@ function App() {
         <h1>TABLET OR LANDSCAPE SMARTPHONE</h1>
       </MediaQuery> */}
       <MediaQuery maxWidth={767}>
-        {/* SMARTPHONE */}
-        <MobileHome />
+          {/* SMARTPHONE */}
+          
+      {windowWidth < 500 ? (
+        <div> Please turn your device to landscape..</div>
+      ) : (<MobileHome />)}
+
+      
+        
       </MediaQuery>
       {/* <MediaQuery minResolution="2dppx">
         {(matches) =>
