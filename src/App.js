f43b10eac5c4892ea916e1de8a35b1c7843f9c91
Amanda Henneberry <amandahenneberry@icloud.com>
2023-07-05 16:10:37 -0400

    media quey test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import './index';
 import Home from './Components/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+
+import { useMediaQuery } from 'react-responsive';
+
+import MediaQuery from 'react-responsive'
+
+// import { DesktopNav, TabletNav, MobileNav } from './components';
 
 
 function App() {
   return (
     <div>
+      <MediaQuery minWidth={992}>
+        <h1>LAPTOP.</h1>
+      </MediaQuery>
+      <MediaQuery minWidth={768} maxWidth={991}>
+        <h1>TABLET OR LANDSCAPE SMARTPHONE</h1>
+      </MediaQuery>
+      <MediaQuery maxWidth={767}>
+        <h1>SMARTPHONE</h1>
+      </MediaQuery>
+      <MediaQuery minResolution="2dppx">
+        {(matches) =>
+        matches
+        ? <p>And you are high DPI</p>
+        : <p>And you are low DPI</p>
+        }
+
+      </MediaQuery>
       <Home />
     </div>
   );
